6d44ddcb02219898d6b80d580c745462d0d87f76
Leejunheezz123 <dotm3434@naver.com>
2022-01-27 13:16:24 +0800

    Navi Clean

--- src/components/inc/NaviCp.js
+++ src/components/inc/NaviCp.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import { css } from "@emotion/react";
 import axios from "axios";
@@ -6,11 +6,20 @@ import axios from "axios";
 const Wrapper = styled.div``;
 
 const NaviCp = () => {
+  const [tree, setTree] = useState([]);
+
   //컴포넌트가 화면에서 실행될때 사라지는 함수.
   useEffect(() => {
     const init = async () => {
-      const { data } = await axios.get("/json/tree.json");
-      console.log(data);
+      const { data } = await axios.get("http://127.0.0.1:3100/api/tree");
+      const tree = data[0].children.map((v) => {
+        let children = v.children.map((v2) => {
+          return { id: v2.id, title: v2.text };
+        });
+        return { id: v.id, title: v.text, children: children };
+      });
+      console.log(tree);
+      setTree(tree);
     };
     init();
     console.log("컴포넌트가 시작됨");
